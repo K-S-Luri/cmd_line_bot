4dcbe03dc5a5f081d5b5969d6b762ca1cc0346c0
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2018-06-10 19:39:38 +0900

    色々と整理

--- prototype/test.py
+++ prototype/test.py
@@ -14,12 +14,13 @@ class CmdLineBot:
     def run(self):
         self.frontend.run(self.call_backend)
     async def call_backend(self, cmdline):
-        res = self.backend.manage_cmdline(cmdline)
-        for r in res:
-            if r["type"] == "msg":
-                await self.frontend.send_msg(channel=r["channel"], text=r["text"])
-            elif r["type"] == "dm":
-                await self.frontend.send_dm(user=r["user"], text=r["text"])
+        tasks = self.backend.manage_cmdline(cmdline)
+        loop = asyncio.get_event_loop()
+        for task in tasks:
+            if task["type"] == "msg":
+                await self.frontend.send_msg(channel=task["channel"], text=task["text"])
+            elif task["type"] == "dm":
+                await self.frontend.send_dm(user=task["user"], text=task["text"])
 
 class CLBFrontEnd(metaclass=ABCMeta):
     def __init__(self):
@@ -63,13 +64,15 @@ class DiscordFrontEnd(CLBFrontEnd):
             print("logged in as")
             print("[name] %s" % client.user.name)
             print("[ id ] %s" % client.user.id)
-            self.server = list(client.servers)[0]
-            self.channel = list(self.server.channels)[0]
+            # self.server = list(client.servers)[0]
+            # self.channel = list(self.server.channels)[0]
             print("------")
     def run(self, callback):
         client = self.client
         @client.event
         async def on_message(msg):
+            if msg.content == "init":
+                self.init_client(msg)
             await callback(msg.content)
         client.run(self.token)
     async def send_msg(self, channel, text):
@@ -77,39 +80,40 @@ class DiscordFrontEnd(CLBFrontEnd):
                                        content=text)
     async def send_dm(self, user, text):
         pass
+    def init_client(self, msg):
+        if msg.server is None:
+            print("initは(DMではなく)テキストチャンネルに送信してください")
+            return
+        print("[Initialize client]\nServer: %s, Channel: %s" % (msg.server, msg.channel))
+        self.server = msg.server
+        self.channel = msg.channel
 
 class MyBackEnd(CLBBackEnd):
     def manage_cmdline(self, cmdline):
         # print("backend: " + cmdline)
         parsed = self.parse_cmdline(cmdline)
         cmd = parsed[0]
+        tasks = []
         if cmd == "dm":
-            return [{"type": "dm",
-                     "user": parsed[1],
-                     "text": parsed[2]}]
+            tasks.append({"type": "dm",
+                          "user": parsed[1],
+                          "text": parsed[2]})
         if cmd == "msg":
-            return [{"type": "msg",
-                     "channel": parsed[1],
-                     "text": parsed[2]},
-                    {"type": "msg",
-                     "channel": parsed[1],
-                     "text": parsed[2]}]
-        return []
+            tasks.append({"type": "msg",
+                          "channel": parsed[1],
+                          "text": parsed[2]})
+        return tasks
     def parse_cmdline(self, cmdline):
         return cmdline.split()
 
 # メイン
 def main():
     token = "NDU1Mjg1OTQwODQ0NDk0ODUw.Df5zIw.Qy0amuaPNJSVSEH30o0H3Gm4e1Y"
+    frontend = MyFrontEnd()
     frontend = DiscordFrontEnd(token)
-    # frontend = MyFrontEnd()
     backend = MyBackEnd()
     clb = CmdLineBot(frontend, backend)
     clb.run()
-
-def _main():
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(async_main())
 
 if __name__ == '__main__':
     main()
