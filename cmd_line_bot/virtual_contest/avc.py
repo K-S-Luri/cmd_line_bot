322029540df254d2f3ae8907bcae351a999a89a0
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2018-06-28 23:55:44 +0900

    AtCoderの問題の情報の取得

--- cmd_line_bot/virtual_contest/avc.py
+++ cmd_line_bot/virtual_contest/avc.py
@@ -3,11 +3,13 @@ from pyquery import PyQuery
 from typing import List, Tuple, Dict, Any, Optional, cast
 import urllib.request
 import os
+import re
 # import yaml
 import json
 
 
 from ..core.clb_data import CLBData
+from ..core.clb_error import CLBError
 
 
 virtual_contest_dir = "virtual_contest"
@@ -35,9 +37,7 @@ class AtCoderAPI:
             return
         with open(self.json_path, "r", encoding="utf-8") as f:
             json_str = f.read()
-        print("loading yaml...")
         self.problems = json.loads(json_str)
-        print("ok")
 
     def search(self, problem_id: str) -> Dict[str, Any]:
         self.read()
@@ -48,8 +48,45 @@ class AtCoderAPI:
         return {}
 
 
+class AtCoderProblem:
+    def __init__(self, url: str, data: CLBData) -> None:
+        self.url = url
+        self.data = data
+        self.set_id()
+        self.set_info()
+
+    def set_id(self) -> None:
+        pattern = r"https://beta\.atcoder\.jp/contests/(.*)/tasks/(.*)"
+        match_obj = re.match(pattern, self.url)
+        if match_obj is None:
+            raise CLBError("問題のURLが不正です: %s" % self.url)
+        self.contest_id = match_obj.group(1)
+        self.problem_id = match_obj.group(2)
+
+    def set_info(self) -> None:
+        api = AtCoderAPI(self.data)
+        self.info = api.search(self.problem_id)  # type: Dict[str, Any]
+
+    def get_title(self) -> str:
+        title = self.info["title"]
+        title = re.sub(r"^[A-Z]\.\s*", "", title)
+        return title
+
+    def get_id(self) -> str:
+        pattern = r"(abc|arc|agc)([0-9]+)_([a-z])"
+        match_obj = re.match(pattern, self.problem_id)
+        if match_obj is None:
+            return self.problem_id
+        contest_type = match_obj.group(1)
+        contest_count = match_obj.group(2)
+        problem_alphabet = match_obj.group(3)
+        return "%s %s %s" % (contest_type.upper(), contest_count, problem_alphabet.upper())
+
+    def get_score(self) -> int:
+        return int(self.info["point"])
+
+
 class AtCoderVirtualContest:
-    # 点数，問題番号などの情報はAtCoderScoresから得るのが良い？
     def __init__(self,
                  contest_id: str,
                  data: CLBData) -> None:
@@ -87,21 +124,31 @@ class AtCoderVirtualContest:
 
 
 def avc_test():
-    contest_id = "6029001596338176"
-    data = CLBData()
-    vc = AtCoderVirtualContest(contest_id, data)
-    # vc.download()
-    problems = vc.get_problems()
     import pprint
-    pprint.pprint(problems)
+    data = CLBData()
+    # contest_id = "6029001596338176"
+    # vc = AtCoderVirtualContest(contest_id, data)
+    # # vc.download()
+    # problems = vc.get_problems()
+    # pprint.pprint(problems)
 
     # scores = AtCoderScores(data)
     # scores.download()
 
-    api = AtCoderAPI(data)
-    # api.download()
+    # api = AtCoderAPI(data)
+    # # api.download()
 
-    query_list = ["arc076_b", "kupc2015_k", "joisc2011_dragon"]
-    for problem_id in query_list:
-        print("searching %s..." % problem_id)
-        pprint.pprint(api.search(problem_id))
+    # query_list = ["arc076_b", "kupc2015_k", "joisc2011_dragon"]
+    # for problem_id in query_list:
+    #     print("searching %s..." % problem_id)
+    #     pprint.pprint(api.search(problem_id))
+
+    urls = ["https://beta.atcoder.jp/contests/cf17-final-open/tasks/cf17_final_b",
+            "https://beta.atcoder.jp/contests/arc076/tasks/arc076_b"]
+    for url in urls:
+        problem = AtCoderProblem(url, data)
+        # pprint.pprint(problem.info)
+        print("%s, %s, %s" % (problem.get_id(),
+                              problem.get_title(),
+                              problem.get_score()))
+        print()
