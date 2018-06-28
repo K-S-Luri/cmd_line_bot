0f61032159c293a8e4faf6deaa958aa3b0c5ddf9
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2018-06-28 11:20:53 +0900

    Scoresや非公式APIからの問題情報の取得

--- cmd_line_bot/virtual_contest/avc.py
+++ cmd_line_bot/virtual_contest/avc.py
@@ -1,10 +1,73 @@
 import imgkit
 from pyquery import PyQuery
-from typing import List, Tuple
+from typing import List, Tuple, Dict, Any, Optional, cast
 import urllib.request
 import os
+import yaml
 
 from ..core.clb_data import CLBData
+
+
+virtual_contest_dir = "virtual_contest"
+
+
+class AtCoderScores:
+    def __init__(self, data: CLBData) -> None:
+        self.url = "https://atcoder-scores.herokuapp.com/index.html"
+        self.data = data
+        self.html_path = os.path.join(self.data.get_category_dir(virtual_contest_dir),
+                                      "scores.html")
+
+    def download(self) -> None:
+        with urllib.request.urlopen(self.url) as response:
+            html = response.read().decode("utf-8")
+        with open(self.html_path, "w", encoding="utf-8") as f:
+            f.write(html)
+
+    def read(self) -> str:
+        with open(self.html_path, "r", encoding="utf-8") as f:
+            html = f.read()
+        return html
+
+
+class AtCoderAPI:
+    def __init__(self, data: CLBData) -> None:
+        self.data = data
+        self.url = "https://kenkoooo.com/atcoder/atcoder-api/info/merged-problems"
+        self.yaml_path = os.path.join(self.data.get_category_dir(virtual_contest_dir),
+                                      "problems.yaml")
+        self.problems = None  # type: Optional[List[Dict[str, Any]]]
+
+    def download(self) -> None:
+        print("downloading...")
+        with urllib.request.urlopen(self.url) as response:
+            print("...")
+            yaml_str = response.read().decode("utf-8")
+        print("loading yaml...")
+        yaml_data = yaml.load(yaml_str)
+        self.problems = yaml_data
+        print("dumping yaml...")
+        yaml_str = yaml.dump(yaml_data)
+        print("saving...")
+        with open(self.yaml_path, "w", encoding="utf-8") as f:
+            f.write(yaml_str)
+
+    def read(self) -> None:
+        if self.problems is not None:
+            return
+        with open(self.yaml_path, "r", encoding="utf-8") as f:
+            yaml_str = f.read()
+        print("loading yaml...")
+        self.problems = yaml.load(yaml_str)
+        print("ok")
+
+    def search(self, problem_id: str) -> Dict[str, Any]:
+        self.read()
+        problems = cast(List[Dict[str, Any]], self.problems)
+        for problem in problems:
+            if problem["id"] == problem_id:
+                return problem
+        return {}
 
 
 class AtCoderVirtualContest:
@@ -14,26 +77,24 @@ class AtCoderVirtualContest:
                  data: CLBData) -> None:
         self.contest_id = contest_id
         self.data = data
+        self.html_path = os.path.join(self.data.get_category_dir(virtual_contest_dir),
+                                      self.contest_id+".html")
 
     def download(self) -> None:
         url = "https://not-522.appspot.com/contest/" + self.contest_id
         with urllib.request.urlopen(url) as response:
             html = response.read().decode("utf-8")
-        html_path = os.path.join(self.data.get_category_dir("virtual_contest"),
-                                 self.contest_id+".html")
-        png_path = os.path.join(self.data.get_category_dir("virtual_contest"),
+        png_path = os.path.join(self.data.get_category_dir(virtual_contest_dir),
                                 self.contest_id+".png")
-        with open(html_path, "w", encoding="utf-8") as f:
-            print("write to %s" % html_path)
+        with open(self.html_path, "w", encoding="utf-8") as f:
+            print("write to %s" % self.html_path)
             f.write(html)
         print("write to %s" % png_path)
         imgkit.from_string(html, png_path)
 
     def read(self) -> str:
         # TODO: htmlファイルが存在しないときの処理とか
-        html_path = os.path.join(self.data.get_category_dir("virtual_contest"),
-                                 self.contest_id+".html")
-        with open(html_path, "r", encoding="utf-8") as f:
+        with open(self.html_path, "r", encoding="utf-8") as f:
             html = f.read()
         return html
 
@@ -55,3 +116,14 @@ def avc_test():
     problems = vc.get_problems()
     import pprint
     pprint.pprint(problems)
+
+    # scores = AtCoderScores(data)
+    # scores.download()
+
+    api = AtCoderAPI(data)
+    # api.download()
+
+    query_list = ["arc076_b", "kupc2015_k", "joisc2011_dragon"]
+    for problem_id in query_list:
+        print("searching %s..." % problem_id)
+        pprint.pprint(api.search(problem_id))
